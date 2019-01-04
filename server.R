a12d630e1efe2d574103e5fae0331a0675bd2f3f
jorgeboucas <mail@jorgeboucas.com>
2019-01-04 10:24:00 +0100

    version control to specific app; test input files links properly resolving

--- server.R
+++ server.R
@@ -1,6 +1,6 @@
 .libPaths("/srv/shiny-server/histogram/libs")
 gitversion <- function(){ 
-  git<-read.csv("/srv/shiny-server/.git/refs/heads/master", header=FALSE)
+  git<-read.csv("/srv/shiny-server/.git/modules/histogram/refs/heads/master", header=FALSE)
   git<-git$V1
   git<-toString(git[1])
   git<-substr(git, 1, 7)
