97dc0c18fe88759c43f292423e92003c9f0272f6
jorgeboucas <mail@jorgeboucas.com>
2019-01-10 08:29:35 +0100

    auto-download example input

--- ui.R
+++ ui.R
@@ -18,7 +18,7 @@ shinyUI( fluidPage(
                                                                                 'semicolon-separated' = ';'), inline = TRUE),
       checkboxInput("header", "Header", TRUE),
       selectInput("column", "Select Column", choices = NULL),
-      helpText(a(href = "https://raw.githubusercontent.com/mpg-age-bioinformatics/shiny-histogram/master/chol.txt", "Example input")),
+      helpText(a(href = "https://datashare.mpcdf.mpg.de/s/TLI22xP5QlVJzRi/download", "Example input")),
       hr(),
       numericInput("lowerx", "X-axis lower limit", NULL),
       numericInput("upperx", "X-axis upper limit", NULL),
